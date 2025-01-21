5f30403edd5fa11627d798e199f19c91e809c15e
Ibrohim <imirxoshimov@gmail.com>
2025-01-21 11:20:46 +0500

    beautify

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,14 +12,14 @@ function HomePage({ setMenu }) {
 
 export default HomePage;
 
-function Products({ setMenu }) {
+function Products() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     fetch("https://api.escuelajs.co/api/v1/products")
       .then((res) => res.json())
       .then((data) => {
-        setProducts(data.slice(0, 10));
+        setProducts(data.slice(5, 17));
       });
   }, []);
 
@@ -27,16 +27,18 @@ function Products({ setMenu }) {
     <div className="grid grid-cols-4 gap-2">
       {products.map((item) => {
         return (
-          <Link key={item.id} to={`/product/${item.id}`}>
-            <img
-              onClick={() => {
-                setMenu("product");
-              }}
-              style={{
-                height: 100,
-              }}
-              src={item.images}
-            />
+          <Link
+            key={item.id}
+            to={`/product/${item.id}`}
+          >
+            <div className="rounded-lg border p-1 shadow">
+              <img
+                className="rounded-md"
+                src={item.images}
+              />
+
+              <h4 className="text-xs">{item.title}</h4>
+            </div>
           </Link>
         );
       })}
