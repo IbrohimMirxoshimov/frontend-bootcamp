b6218b05d14690a7bacb6312e21504eb1c5931ae
Ibrohim <imirxoshimov@gmail.com>
2025-01-27 08:10:00 +0500

    book uz API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
 import "./style/index.css";
+import axios from "axios";
+import { Link } from "react-router";
 
 function App() {
-  return <div className="w-full m-auto container p-4">Todo</div>;
+  const [kitob, setKitob] = useState();
+  useEffect(() => {
+    axios
+      .get(
+        "https://backend.book.uz/user-api/book/hazrati-abu-bakr-roziyallohu-anhu-1"
+      )
+      .then((response) => {
+        setKitob(response.data);
+      });
+  }, []);
+
+  if (!kitob) {
+    return <>Yuklanmoqda...</>;
+  }
+
+  return (
+    <Link to={`book/${kitob.data._id}`}>
+      <div>
+        <h2>{kitob.data.name}</h2>
+        <img src={`https://backend.book.uz/user-api/${kitob.data.imgUrl}`} />
+      </div>
+    </Link>
+  );
 }
 
 export default App;
