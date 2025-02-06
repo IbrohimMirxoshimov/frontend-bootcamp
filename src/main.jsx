95a6d1df5783ad9ea0a02539412b47149758e4ce
Ibrohim <imirxoshimov@gmail.com>
2025-02-06 11:38:23 +0500

    product cart with zustand

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router";
 import App from "./App.jsx";
 import "./style/index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
