6dc22aec225a9935a477128032b22e3969dfb256
Ibrohim <imirxoshimov@gmail.com>
2025-02-19 08:30:14 +0500

    extract Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import React, { useState } from "react";
-import { Link, Route, Routes } from "react-router";
+import React from "react";
+import { Route, Routes } from "react-router";
+import CatalogPage from "./pages/CatalogPage";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
-import CatalogPage from "./pages/CatalogPage";
-import { useEffect } from "react";
-import axios from "axios";
-import { Button } from "antd";
-import useMyStore from "./my-store";
+import Navbar from "./components-mini-shop/Navbar";
 
 function App() {
   return (
@@ -30,49 +27,3 @@ function App() {
   );
 }
 export default App;
-
-function Navbar() {
-  const state = useMyStore();
-  return (
-    <nav className="mb-4">
-      <div className="flex items-center justify-between mb-2">
-        <Link
-          to={"/"}
-          className="text-2xl bg-blue-300 rounded px-2"
-        >
-          Logo
-        </Link>
-        <Button type="primary">Savat {state.savatcha.length}</Button>
-      </div>
-      <TopMenu />
-    </nav>
-  );
-}
-
-function TopMenu() {
-  const [categories, setCategories] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get(`https://gw.texnomart.uz/api/web/v1/header/top-categories`)
-      .then((res) => {
-        setCategories(res.data.data.data);
-      });
-  }, []);
-
-  return (
-    <div className="flex gap-2 overflow-x-auto">
-      {categories.map((item) => {
-        return (
-          <Link
-            key={item.slug}
-            to={`/katalog/${item.slug}`}
-            className="text-xl bg-slate-200 rounded px-2"
-          >
-            {item.title}
-          </Link>
-        );
-      })}
-    </div>
-  );
-}
---
+++ src/components-mini-shop/Navbar.jsx
@@ -0,0 +1,51 @@
+import { Button } from "antd";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router";
+import useMyStore from "../my-store";
+
+export default function Navbar() {
+  const state = useMyStore();
+  return (
+    <nav className="mb-4">
+      <div className="flex items-center justify-between mb-2">
+        <Link
+          to={"/"}
+          className="text-2xl bg-blue-300 rounded px-2"
+        >
+          Logo
+        </Link>
+        <Button type="primary">Savat {state.savatcha.length}</Button>
+      </div>
+      <TopMenu />
+    </nav>
+  );
+}
+
+function TopMenu() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://gw.texnomart.uz/api/web/v1/header/top-categories`)
+      .then((res) => {
+        setCategories(res.data.data.data);
+      });
+  }, []);
+
+  return (
+    <div className="flex gap-2 overflow-x-auto">
+      {categories.map((item) => {
+        return (
+          <Link
+            key={item.slug}
+            to={`/katalog/${item.slug}`}
+            className="text-xl bg-slate-200 rounded px-2"
+          >
+            {item.title}
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
